5af0522c922efc270dde33544aa74b17cbb2dae9
paul chau <paul@chau>
2023-05-30 22:11:02 -0400

    added comment header

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: On Hei Chau, Paul  
+*  Student ID: 172917213 
+*  Date: May 31 2023
+*
+*  Cyclic Web App URL: https://ill-calf-neckerchief.cyclic.app
+*
+*  GitHub Repository URL: https://github.com/onheichau/web322
+*
+********************************************************************************/ 
+
 const blog = require('./blog-service');
 const path = require('path')
 const express = require('express');
